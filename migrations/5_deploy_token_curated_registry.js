490ad5e4de917dda452bc90f32c78f8cace9e6e4
Silke Van den Broeck <silkevandenbroeck@Silkes-MacBook-Pro.local>
2018-03-12 16:29:41 +0100

    fix: added api calls in separate helper file for migrations

--- migrations/5_deploy_token_curated_registry.js
+++ migrations/5_deploy_token_curated_registry.js
@@ -1,26 +1,16 @@
-const axios = require('axios')
-
 const { createPermission, grantPermission } = require('./helpers/permissions')
+const {
+  GATEWAY_OPERATOR_ADDRESS,
+  GATEWAY_OPERATOR_PRIVATE_KEY,
+  getBaseUrl,
+  authenticate,
+  addIpfs,
+} = require('./helpers/api')
 
 const StreamRegistry = artifacts.require('StreamRegistry.sol')
 const StreamFactory = artifacts.require('StreamFactory.sol')
 const Token = artifacts.require('DtxToken.sol')
 const GateKeeper = artifacts.require('GateKeeper')
-
-const GATEWAY_OPERATOR_PRIVATE_KEY =
-  'ca1398820695e93cea849b841a9aae4eeae65518d14353ab73d21fa4af2d58a7'
-const GATEWAY_OPERATOR_ADDRESS = '0x3df2fd51cf19c0d8d1861d6ebc6457a1b0c7496f'
-
-function getBaseUrl(network) {
-  switch (network) {
-    case 'development':
-      return 'http://localhost:3333'
-    case 'minttestnet':
-      return 'https://dapi-staging.databrokerdao.com'
-    default:
-      return 'http://localhost:3333'
-  }
-}
 
 async function enlistStream(deployer, network, accounts) {
   const registry = await StreamRegistry.deployed()
@@ -52,41 +42,6 @@ async function enlistStream(deployer, network, accounts) {
   await registry.enlist('10', '10', ipfsHash || '', {
     from: accounts[0],
   })
-}
-
-async function authenticate(network) {
-  try {
-    const res = await axios({
-      url: `${getBaseUrl(network)}/authenticate`,
-      method: 'POST',
-      data: {
-        privateKeys: {
-          ethereum: GATEWAY_OPERATOR_PRIVATE_KEY,
-        },
-        encrypted: false,
-      },
-    })
-
-    return res.data.token
-  } catch (e) {
-    console.log('Authenticate failed: ', e)
-  }
-}
-
-async function addIpfs(metadata, token, network) {
-  try {
-    const res = await axios({
-      url: `${getBaseUrl(network)}/ipfs/add/json`,
-      method: 'POST',
-      data: metadata,
-      headers: {
-        Authorization: token,
-      },
-    })
-    return res.data[0].hash
-  } catch (e) {
-    console.log('AddIpfs failed: ', e)
-  }
 }
 
 async function deployRegistry(deployer, network, accounts) {
---
+++ migrations/helpers/api.js
@@ -0,0 +1,59 @@
+const axios = require('axios')
+
+const GATEWAY_OPERATOR_PRIVATE_KEY =
+  'ca1398820695e93cea849b841a9aae4eeae65518d14353ab73d21fa4af2d58a7'
+const GATEWAY_OPERATOR_ADDRESS = '0x3df2fd51cf19c0d8d1861d6ebc6457a1b0c7496f'
+
+function getBaseUrl(network) {
+  switch (network) {
+    case 'development':
+      return 'http://localhost:3333'
+    case 'minttestnet':
+      return 'https://dapi-staging.databrokerdao.com'
+    default:
+      return 'http://localhost:3333'
+  }
+}
+
+async function authenticate(network) {
+  try {
+    const res = await axios({
+      url: `${getBaseUrl(network)}/authenticate`,
+      method: 'POST',
+      data: {
+        privateKeys: {
+          ethereum: GATEWAY_OPERATOR_PRIVATE_KEY,
+        },
+        encrypted: false,
+      },
+    })
+
+    return res.data.token
+  } catch (e) {
+    console.log('Authenticate failed: ', e)
+  }
+}
+
+async function addIpfs(metadata, token, network) {
+  try {
+    const res = await axios({
+      url: `${getBaseUrl(network)}/ipfs/add/json`,
+      method: 'POST',
+      data: metadata,
+      headers: {
+        Authorization: token,
+      },
+    })
+    return res.data[0].hash
+  } catch (e) {
+    console.log('AddIpfs failed: ', e)
+  }
+}
+
+module.exports = {
+  GATEWAY_OPERATOR_ADDRESS,
+  GATEWAY_OPERATOR_PRIVATE_KEY,
+  getBaseUrl,
+  authenticate,
+  addIpfs,
+}
