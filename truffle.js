54ad9fbc504e3a296551d6949790f2f1c0be1b3b
Roderik van der Veer <roderik@settlemint.com>
2018-06-29 12:31:20 +0200

    fix: skip the extra function

--- truffle.js
+++ truffle.js
@@ -9,72 +9,50 @@ module.exports = {
   },
   networks: {
     development: {
-      provider: () => {
-        return new HDWalletProvider(
-          process.env.ETHEREUM_DEPLOYER_SEED ||
-            'robot robot robot robot robot robot robot robot robot robot robot robot',
-          'http://localhost:8545'
-        )
-      },
+      provider: new HDWalletProvider(
+        process.env.ETHEREUM_DEPLOYER_SEED ||
+          'robot robot robot robot robot robot robot robot robot robot robot robot',
+        'http://localhost:8545'
+      ),
       gasPrice: 0x00,
       network_id: '1337',
     },
     mintnet: {
-      provider: () => {
-        return new HDWalletProvider(
-          process.env.ETHEREUM_DEPLOYER_SEED,
-          'https://mintnet.settlemint.com'
-        )
-      },
+      provider: new HDWalletProvider(
+        process.env.ETHEREUM_DEPLOYER_SEED,
+        'https://mintnet.settlemint.com'
+      ),
       gasPrice: 0x00,
       network_id: '8995',
     },
     minttestnet: {
-      provider: () => {
-        return new HDWalletProvider(
-          process.env.ETHEREUM_DEPLOYER_SEED,
-          'https://minttestnet.settlemint.com'
-        )
-      },
+      provider: new HDWalletProvider(
+        process.env.ETHEREUM_DEPLOYER_SEED,
+        'https://minttestnet.settlemint.com'
+      ),
       gasPrice: 0x00,
       network_id: '8996',
     },
-    mintdemonet: {
-      provider: () => {
-        return new HDWalletProvider(
-          process.env.ETHEREUM_DEPLOYER_SEED,
-          'https://mintdemonet.settlemint.com'
-        )
-      },
-      gasPrice: 0x00,
-      network_id: '8997',
-    },
     kovan: {
-      provider: () => {
-        return new HDWalletProvider(
-          process.env.ETHEREUM_DEPLOYER_SEED,
-          'https://kovan.infura.io/'
-        )
-      },
+      provider: new HDWalletProvider(
+        process.env.ETHEREUM_DEPLOYER_SEED,
+        'https://kovan.infura.io/'
+      ),
       network_id: '42',
       gas: 4700000,
     },
     ropsten: {
-      provider: () => {
-        return new HDWalletProvider(
-          process.env.ETHEREUM_DEPLOYER_SEED,
-          'https://ropsten.infura.io/'
-        )
-      },
+      provider: new HDWalletProvider(
+        process.env.ETHEREUM_DEPLOYER_SEED,
+        'https://ropsten.infura.io/'
+      ),
       network_id: '3',
     },
     rinkeby: {
-      provider: () => {
-        return new HDWalletProvider(
-          process.env.ETHEREUM_DEPLOYER_SEED,
-          'https://rinkeby.infura.io/'
-        )
-      },
+      provider: new HDWalletProvider(
+        process.env.ETHEREUM_DEPLOYER_SEED,
+        'https://rinkeby.infura.io/'
+      ),
       network_id: '4',
     },
     coverage: {
